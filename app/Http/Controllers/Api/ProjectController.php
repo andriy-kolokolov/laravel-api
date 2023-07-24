06aa2a188661b11bc240f14b4b08d324f9c54a6b
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-24 17:06:09 +0200

    add per_page parameter to handle better pagination side frontEnd

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project\Project;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(): JsonResponse {
+    public function index(Request $request): JsonResponse {
+        $perPage = $request->query('per_page', 8);
+
         $projects = Project::with('type', 'programmingLanguages', 'technologies')
-            ->paginate(6);
+            ->paginate($perPage);
         return response()->json($projects);
     }
 
